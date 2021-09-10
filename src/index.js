58480b525da97c6886e489071304982454ed90c6
Roman <ayoz4@yandex.ru>
2021-09-10 16:00:18 +0700

    added some loaders

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./styles/test.css";
+import "./styles/scss.scss";
 
 console.log("Test 1");
 console.log("Test 1");
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,29 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isDev = process.env.NODE_ENV === "development";
+const isProd = !isDev;
+
+const cssLoaders = (extra) => {
+  const loaders = [
+    {
+      loader: MiniCssExtractPlugin.loader,
+      options: {
+        // hmr: isDev,
+        // reloadAll: true,
+      },
+    },
+    "css-loader",
+  ];
+
+  if (extra) {
+    loaders.push(extra);
+  }
+
+  return loaders;
+};
 
 module.exports = {
   context: path.resolve(__dirname, "src"),
@@ -10,17 +33,35 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  // resolve: {
+  //   extenstions: [".js", ".json", ".png"],
+  //   alias: {
+
+  //   }
+  // },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./index.html",
+      minify: {
+        collapseWhitespace: isProd,
+      },
     }),
     new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" }),
   ],
+  devServer: {
+    port: 3000,
+    hot: isDev,
+  },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: cssLoaders(),
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: cssLoaders("sass-loader"),
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
@@ -30,6 +71,10 @@ module.exports = {
         },
         type: "javascript/auto",
         dependency: { not: ["url"] },
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        use: ["file-loader"],
       },
     ],
   },
